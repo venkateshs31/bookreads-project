668c5cdd9f5a34ff3ae33bf484ed7d6f16c3553a
venkateshs31 <116935669+venkateshs31@users.noreply.github.com>
2024-10-09 12:13:12 +0530

    Update BannerCard.jsx

--- frontend/src/home/BannerCard.jsx
+++ frontend/src/home/BannerCard.jsx
@@ -11,6 +11,12 @@ import './BannerCard.css';
 // import required modules
 import { EffectCards } from 'swiper/modules';
 
+import book1 from '../assets/banner-books/book1.png';
+import book2 from '../assets/banner-books/book2.png';
+import book3 from '../assets/banner-books/book3.png';
+import book4 from '../assets/banner-books/book4.png';
+import book5 from '../assets/banner-books/book5.png';
+
 const BannerCard = () => {
   return (
     <div className='banner'>
@@ -20,11 +26,27 @@ const BannerCard = () => {
         modules={[EffectCards]}
         className="mySwiper"
       >
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
+        <SwiperSlide>
+          <img src={book1} alt="Book 1" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <img src={book2} alt="Book 2" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <img src={book3} alt="Book 3" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <img src={book4} alt="Book 4" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <img src={book5} alt="Book 5" />
+        </SwiperSlide>
+        
+      </Swiper>
         
       </Swiper>
       </div>
